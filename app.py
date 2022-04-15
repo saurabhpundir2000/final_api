9135a93591baf6fe3141afc3b3b21c9bef992fcc
saurabh pundir <45098115+saurabhpundir2000@users.noreply.github.com>
2022-04-15 11:06:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+### import the packages
+from flask import Flask, jsonify, request
+# import requests, json
+# instantiate the class
+app = Flask(__name__)
+# setting up routes for methods to be accessed via call requests
+@app.route('/cal', methods=['POST'])
+def gather():
+    data = request.get_json() # fetch the payload and 
+    output = None
+    height = data['height']         # desearlize it and capture the data into identifiers
+    weight = data['weight']
+    output = weight/pow(height,2)
+    return(jsonify({"BMI": output}))
+
+if __name__ == '__main__':
+    app.run(debug=True)
